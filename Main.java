aff29d0d49a4d88daabb869b2510ef4031dc7856
buseozkan <buse1.ozkan@gmail.com>
2022-10-12 16:06:55 +0100

    java date

---
+++ Main.java
@@ -0,0 +1,11 @@
+// import java.time.LocalDateTime;
+// import java.time.DateTimeFormatter;
+import java.time.*;
+
+public class Mafin {
+   
+     public static void main(String[] args) {
+    LocalDate myDate = LocalDate.now();
+    System.out.println(myDate);
+    }
+}
