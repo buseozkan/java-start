b4baf3082f616199a72a0ca583cbbd857a98144d
buseozkan <buse1.ozkan@gmail.com>
2022-10-12 17:04:04 +0100

    arraylist

--- Main.java
+++ Main.java
@@ -1,14 +1,26 @@
 
 import java.time.*;
 import java.time.format.*;
+import java.util.ArrayList;
+
 public class Main {
    
-     public static void main(String[] args) {
+ public static void main(String[] args) {
     LocalDate myDate = LocalDate.now();
-    System.out.println(myDate);
+    System.out.println(myDate + " unformated date");
 
     DateTimeFormatter myTime = DateTimeFormatter.ofPattern("dd-MM-yyyy");
     String formattedDate = myDate.format(myTime);
-        System.out.println(formattedDate);
+    System.out.println(formattedDate + " formatted date");
+ 
+ ArrayList<String> colors = new ArrayList<String>();
+ colors.add("red");
+ colors.add("green");
+ colors.add("blue");
+ colors.add("yellow");
+ colors.add("magenta");
+ colors.add("cyan");
+ for (int i = 0; i < 5; i++) {System.out.println(colors.get(i));}
+ System.out.println(colors.size());
     }
 }
