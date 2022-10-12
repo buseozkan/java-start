d07a0defe654b90fcca0714bda8cb58b43a2d644
buseozkan <buse1.ozkan@gmail.com>
2022-10-12 16:57:05 +0100

    formatted date

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
-// import java.time.LocalDateTime;
-// import java.time.DateTimeFormatter;
-import java.time.*;
 
-public class Mafin {
+import java.time.*;
+import java.time.format.*;
+public class Main {
    
      public static void main(String[] args) {
     LocalDate myDate = LocalDate.now();
     System.out.println(myDate);
+
+    DateTimeFormatter myTime = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    String formattedDate = myDate.format(myTime);
+        System.out.println(formattedDate);
     }
 }
