cbdf3c77c903e8b0041faca8471cd0cdeae1f423
buseozkan <buse1.ozkan@gmail.com>
2022-10-13 13:57:44 +0100

    folder destructure

---
+++ learning/if/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        if(4==4 && 5 != 0) {
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+    }
+}
