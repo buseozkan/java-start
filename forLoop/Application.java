e191c6274eb1c7ec23d65c7f26fc5cbae30dee03
buseozkan <buse1.ozkan@gmail.com>
2022-10-13 12:36:26 +0100

    relined comment

--- forLoop/Application.java
+++ forLoop/Application.java
@@ -1,6 +1,7 @@
 public class Application {
     public static void main(String[] args) {
-        // 3 sections of for (int i = 0; i < 5; i++) where i is initialized, and a limit is specified eg i<5,
+        // 3 sections of for (int i = 0; i < 5; i++) where
+        // i is initialized, and a limit is specified eg i<5,
      for(int i = 0; i < 5; i++) {
         
      
