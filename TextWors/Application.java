0ede3c82228b87108332bd450c1a0d7c0788335a
buseozkan <buse1.ozkan@gmail.com>
2022-10-13 12:12:12 +0100

    text variables

---
+++ TextWors/Application.java
@@ -0,0 +1,11 @@
+public class Application {
+    public static void main(String[] args) {
+        //String is a type of object which can hold class
+        int myInt = 7;
+        String text = "Hallo";
+        String blank = " ";
+        String name = "world";
+        String greeting = text + blank + name;
+        System.out.println(greeting);
+    }
+}
